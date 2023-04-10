7728adee69834a2147b83b3bd69356255b80b34c
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-04-10 07:51:27 +0530

    product table html done and load product data to the dable

--- src/app/components/seller-home/seller-home.component.ts
+++ src/app/components/seller-home/seller-home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { product } from 'src/app/models/data-type';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
   styleUrls: ['./seller-home.component.css']
 })
-export class SellerHomeComponent {
+export class SellerHomeComponent implements OnInit {
+  productList:undefined | product[]
+
+  constructor(private productService:ProductService){}
+
+  ngOnInit():void{
+    this.productService.productList().subscribe((res)=>{
+      if(res){
+        this.productList =res
+      }
+    })
+  }
 
 }
