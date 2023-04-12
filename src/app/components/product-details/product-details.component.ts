bce210c6ef86ff33ddbbaea8ff705eeab71339a6
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-04-12 08:37:31 +0530

    get product details to productData variable

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { product } from 'src/app/models/data-type';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
+  productData:undefined|product
+
+  constructor(private activatedRoute:ActivatedRoute,private productService:ProductService) {}
+
+  ngOnInit():void{
+    let productId= this.activatedRoute.snapshot.paramMap.get('id')
+    productId && this.productService.getProduct(productId).subscribe((res)=>{
+      this.productData = res
+    })
+
+  }
 
 }
