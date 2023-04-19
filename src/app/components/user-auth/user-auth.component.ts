88c2a80d4b00a282729a2b983bad8994182bbd27
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-04-19 08:39:41 +0530

    toggle between user login and signup done

--- src/app/components/user-auth/user-auth.component.ts
+++ src/app/components/user-auth/user-auth.component.ts
@@ -5,21 +5,38 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-user-auth',
   templateUrl: './user-auth.component.html',
-  styleUrls: ['./user-auth.component.css']
+  styleUrls: ['./user-auth.component.css'],
 })
 export class UserAuthComponent implements OnInit {
+  showLogin: boolean = true;
+  authError: string = '';
+  constructor(private userService: UserService) {}
 
-  showLogin:boolean=true
-  constructor(private userService:UserService){}
-
-  ngOnInit():void{}
-
-  signUp(data:signUp){
-    this.userService.userSignUp(data)
+  ngOnInit(): void {
+    this.userService.userAuthReload();
   }
 
-  openLogin(){
-    this.showLogin=true;
+  signUp(data: signUp) {
+    this.userService.userSignUp(data);
+  }
+
+  openSignUp() {
+    this.showLogin = false;
+  }
+  openLogin() {
+    this.showLogin = true;
+  }
+
+/*   login(data: login) {
+    this.userService.userLogin(data)
+    this.userService.invalidUserAuth.subscribe((result)=>{
+      console.warn(result);
+      if(result){
+         this.authError="User not found"
+      }else{
+        this.localCartToRemoteCart();
       }
 
+    })
+  } */
 }
