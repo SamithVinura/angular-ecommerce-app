359643617a020ff6ea47c05bd17715a2cbea5c02
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-04-19 07:58:24 +0530

    create user sign up function in service file

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { signUp } from '../models/data-type';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  userSignUp(user:signUp){
+    this.http.post("http://localhost:4000/user",user)
+  }
 }
