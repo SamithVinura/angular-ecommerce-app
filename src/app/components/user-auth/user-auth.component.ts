658d2f097156d41f21d123977e08754b3d6cfddd
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-04-20 08:07:10 +0530

    fixing some bugs in user html

--- src/app/components/user-auth/user-auth.component.ts
+++ src/app/components/user-auth/user-auth.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { signUp } from 'src/app/models/data-type';
+import { login, signUp } from 'src/app/models/data-type';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -27,16 +27,15 @@ export class UserAuthComponent implements OnInit {
     this.showLogin = true;
   }
 
-/*   login(data: login) {
+  login(data: login) {
+    console.log(data)
     this.userService.userLogin(data)
-    this.userService.invalidUserAuth.subscribe((result)=>{
-      console.warn(result);
-      if(result){
-         this.authError="User not found"
+    this.userService.invalidUserAuth.subscribe((val)=>{
+      if(val){
+        this.authError = "User not found"
       }else{
-        this.localCartToRemoteCart();
+        this.authError = ''
       }
-
     })
-  } */
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { signUp } from '../models/data-type';
+import { EventEmitter, Injectable } from '@angular/core';
+import { login, signUp } from '../models/data-type';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class UserService {
+  invalidUserAuth = new EventEmitter<boolean>(false)
 
   constructor(private http:HttpClient,private router:Router) { }
 
@@ -24,5 +25,20 @@ export class UserService {
     if(localStorage.getItem('user')){
       this.router.navigate(['/'])
     }
+  }
+
+  userLogin(data:login){
+    console.log(data)
+    this.http.get<signUp[]>(`http://localhost:4000/user?email=${data.email}&password=${data.password}`,
+    {observe:'response'}
+    ).subscribe((result)=>{
+      if(result && result.body?.length){
+        localStorage.setItem('user',JSON.stringify(result.body[0]));
+        this.router.navigate(['/']);
+        this.invalidUserAuth.emit(false)
+      }else{
+        this.invalidUserAuth.emit(true)
+      }
+    })
   }
 }
