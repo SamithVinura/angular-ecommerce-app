1b91cfa48871ae02b367a19c3a49379ea85f386d
Samith Vinura <104480599+SamithVinura@users.noreply.github.com>
2023-04-25 07:53:00 +0530

    showing actual total price in html

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
+  totalPrice: number | undefined;
 
-  constructor(){}
+  constructor(private productService: ProductService, private router: Router) {}
 
-  ngOnInit():void{
-
+  ngOnInit(): void {
+    this.productService.currentCart().subscribe((res) => {
+      let price = 0;
+      res.forEach((item) => {
+        if (item.quantity) {
+          price = price + +item.price * item.quantity;
+        }
+      });
+      this.totalPrice = price + price / 10 + 100 - price / 10;
+    });
   }
 
-  orderNow(data:any){
-
-  }
+  orderNow(data: any) {}
 }
